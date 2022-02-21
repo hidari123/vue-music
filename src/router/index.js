a653fe800811c78f3ad3ba60f72568b890e38928
lijiaying <1640106564@qq.com>
2022-02-21 11:28:56 +0800

    初始化&创建视图容器

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Layout from '@/views/Layout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'Layout',
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/topic',
+        name: 'Topic',
+        // component: Topic
+        // 异步加载方案 什么时候需要什么时候加载
+        component: () => import('../views/Topic.vue')
+      },
+      {
+        path: '/mine',
+        name: 'Mine',
+        component: () => import('../views/Mine.vue')
+      },
+      {
+        path: '/search',
+        name: 'Search',
+        component: () => import('../views/Search.vue')
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
+  mode: 'hash',
+  base: process.env.BASE_URL,
   routes
 })
 
---
+++ src/views/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+<!-- 视图容器 -->
+    <div>
+        <Nav></Nav>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import Nav from '../components/nav.vue'
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style>
+
+</style>
